ec33933ab0368988b132061ae87da670d6ee298c
AugustinCombes <65305236+AugustinCombes@users.noreply.github.com>
2024-11-23 17:25:53 +0100

    Update setup.py to remove deprecated pip imports

--- setup.py
+++ setup.py
@@ -1,34 +1,16 @@
-"""This module contains the packaging routine for the pybook package"""
-
 from setuptools import setup, find_packages
-try:
-    from pip.download import PipSession
-    from pip.req import parse_requirements
-except ImportError:
-    # It is quick hack to support pip 10 that has changed its internal
-    # structure of the modules.
-    from pip._internal.download import PipSession
-    from pip._internal.req.req_file import parse_requirements
-
-
-def get_requirements(source):
-    """Get the requirements from the given ``source``
-
-    Parameters
-    ----------
-    source: str
-        The filename containing the requirements
-
-    """
-
-    install_reqs = parse_requirements(filename=source, session=PipSession())
-
-    return [str(ir.req) for ir in install_reqs]
-
 
 setup(
+    name='scrapy-selenium',
+    version='0.0.7',
     packages=find_packages(),
-    install_requires=get_requirements('requirements/requirements.txt')
+    install_requires=[
+        'scrapy>=1.4.0',
+        'selenium',
+    ],
+    url='https://github.com/AugustinCombes/scrapy-selenium',
+    license='MIT',
+    author='Augustin Combes',
+    author_email='',
+    description='Scrapy middleware to handle javascript pages using selenium'
 )
-
-
